90cc2959eb90131ed929d306f25659bc3399d515
Max Edwards <youwontforgetthis@gmail.com>
2012-07-18 21:34:56 +0100

    Updated tests to mspec

---
+++ Noble.Tests/Specs/BrowserTestBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using System.Dynamic;
+using Nancy;
+using Nancy.Testing;
+
+
+namespace Noble.Tests.Specs
+{
+    public class BrowserTestBase
+    {
+        protected static DefaultNancyBootstrapper bootstrapper;
+        protected static Browser browser;
+        protected static BrowserResponse response;
+
+        protected Establish a = () =>
+        {
+            bootstrapper = new TestBootstrapper();
+            browser = new Browser(bootstrapper);
+        };
+
+    }
+}
---
+++ Noble.Tests/Specs/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using System.Dynamic;
+using Nancy;
+using Nancy.Testing;
+
+namespace Noble.Tests.Specs
+{
+    //Subject/It/Because of/Establish context/Cleanup after
+    [Subject("Login")]
+    public class When_using_correct_credentials : BrowserTestBase
+    {
+        
+        Because of = () => response = browser.Post("/login/", (with) =>
+            {
+                with.HttpRequest();
+                with.FormValue("Username", "username");
+                with.FormValue("Password", "wrongpassword");
+            });
+
+        It should_display_a_login_screen =
+            () =>
+            {
+                response.ShouldHaveRedirectedTo("/login?error=true&username=username");
+            };
+
+        //Cleanup after = () => ExpandoObject = null;
+    }
+}
