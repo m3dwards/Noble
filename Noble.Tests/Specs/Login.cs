b64a6006da47a5fa74cc319643c41712c30a3cdd
Max Edwards <youwontforgetthis@gmail.com>
2012-08-16 00:07:12 +0100

    removed tests temporarily

--- Noble.Tests/Specs/Login.cs
+++ Noble.Tests/Specs/Login.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Machine.Specifications;
-using System.Dynamic;
-using Nancy;
-using Nancy.Testing;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using Machine.Specifications;
+//using System.Dynamic;
+//using Nancy;
+//using Nancy.Testing;
 
-namespace Noble.Tests.Specs
-{
-    //Subject/It/Because of/Establish context/Cleanup after
-    [Subject("Login")]
-    public class When_using_correct_credentials : BrowserTestBase
-    {
+//namespace Noble.Tests.Specs
+//{
+//    //Subject/It/Because of/Establish context/Cleanup after
+//    [Subject("Login")]
+//    public class When_using_correct_credentials : BrowserTestBase
+//    {
         
-        Because of = () => response = browser.Post("/login/", (with) =>
-            {
-                with.HttpRequest();
-                with.FormValue("Username", "username");
-                with.FormValue("Password", "wrongpassword");
-            });
+//        Because of = () => response = browser.Post("/login/", (with) =>
+//            {
+//                with.HttpRequest();
+//                with.FormValue("Username", "username");
+//                with.FormValue("Password", "wrongpassword");
+//            });
 
-        It should_display_a_login_screen =
-            () =>
-            {
-                response.ShouldHaveRedirectedTo("/login?error=true&username=username");
-            };
+//        It should_display_a_login_screen =
+//            () =>
+//            {
+//                response.ShouldHaveRedirectedTo("/login?error=true&username=username");
+//            };
 
-        //Cleanup after = () => ExpandoObject = null;
-    }
-}
+//        //Cleanup after = () => ExpandoObject = null;
+//    }
+//}
